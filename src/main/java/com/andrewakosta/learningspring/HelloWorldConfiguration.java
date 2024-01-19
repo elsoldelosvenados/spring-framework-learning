c51b7b063762a6ff515acbfe5a46ee1fda036492
Andres Acosta <andresakosta@outlook.com>
2024-01-18 19:12:03 -0500

    18 Implementing auto wiring in Java Spring framework configuration file

--- src/main/java/com/andrewakosta/learningspring/HelloWorldConfiguration.java
+++ src/main/java/com/andrewakosta/learningspring/HelloWorldConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-record Person(String name, int age) {};
+record Person(String name, int age, Address address) {};
 record Address(String firstLine, String city) {};
 @Configuration
 public class HelloWorldConfiguration {
@@ -19,11 +19,25 @@ public class HelloWorldConfiguration {
     }
     @Bean
     public Person person(){
-        return new Person("Andrew", 22);
+        return new Person("Andrew", 22, new Address("123 Main St", "New York"));
     }
 
     @Bean
+    public Person person2MethodCall(){
+        return new Person(name(), age(), address());
+    }
+    @Bean
+    public Person person3Parameters(String name, int age, Address address3){
+        return new Person(name, age, address3);
+    }
+    @Bean(name = "address2")
     public Address address(){
         return new Address("123 Main St", "New York");
     }
+
+    @Bean
+    public Address address3(){
+        return new Address("Baker Street", "London");
+    }
+
 }
